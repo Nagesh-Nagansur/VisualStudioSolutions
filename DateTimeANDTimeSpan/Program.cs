67cf7a43bf400ef631a1d8fd10f53fce85956e54
Nagesh Nagansur <nageshnagansur@gmail.com>
2020-04-12 12:23:15 +0530

    Updated

--- DateTimeANDTimeSpan/Program.cs
+++ DateTimeANDTimeSpan/Program.cs
@@ -11,37 +11,59 @@ namespace DateTimeANDTimeSpan
         static void Main(string[] args)
         {
             // DateTime
-           
+
             DateTime dt = new DateTime();
             Console.WriteLine(dt);
-            
 
-            DateTime date = new DateTime(2020,04,10);
+
+            DateTime date = new DateTime(2020, 04, 10);
             var today = DateTime.Now;
             Console.WriteLine(today);
-
             var tommorow = DateTime.Now.AddDays(1);
             Console.WriteLine(tommorow);
 
-            var after2hour= DateTime.Now.AddHours(2);
+            var after2hour = DateTime.Now.AddHours(2);
             Console.WriteLine(after2hour);
             Console.WriteLine(today.ToString("mm/dd/yy"));
             Console.WriteLine(today.ToString("mm/dd/yy hh:mm:ss"));
             Console.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
 
-            DateTime start = new DateTime(2020, 04, 10);
-            DateTime end = new DateTime(2020, 04, 14);
+
+
+
+
 
             //TimeSpan 
+
+            DateTime start = DateTime.Now;
+            DateTime end = new DateTime(2020, 04, 30);
+
             TimeSpan time = new TimeSpan();
+            Console.WriteLine(time);
             time = end - start;
-            
+
             Console.WriteLine(time.ToString());
-           
 
-   
+            Console.WriteLine(time.Days);
+            Console.WriteLine(time.TotalDays);
+            Console.WriteLine(time.Hours);
+            Console.WriteLine(time.TotalHours);
+            Console.WriteLine(time.Minutes);
+            Console.WriteLine(time.TotalMinutes);
+            Console.WriteLine(time.Seconds);
+            Console.WriteLine(time.TotalSeconds);
 
-          
+            //Static fields
+
+            var t1 = TimeSpan.FromSeconds(300);
+            var t2 = TimeSpan.FromMinutes(90);
+            Console.WriteLine(TimeSpan.FromHours(1.25));
+            Console.WriteLine(TimeSpan.Compare(t1, t2));
+            Console.WriteLine(TimeSpan.Equals(t1, t2));
+
+
+
+
 
 
 
