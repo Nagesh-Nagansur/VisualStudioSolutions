b2f7d2d4d619332dc9f5f314169104fec6b749ec
Nagesh Nagansur <nageshnagansur@gmail.com>
2020-04-12 10:54:27 +0530

    Modified

--- ReadTextFile/Program.cs
+++ ReadTextFile/Program.cs
@@ -10,30 +10,33 @@ namespace ReadTextFile
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             var path = @"D:\Roof_Work\text.txt";
             Console.WriteLine("Write the text");
             var Read = Console.ReadLine();
             File.WriteAllText(path, Read);
-            StreamReader read = new StreamReader(path);
-            var line = read.ReadToEnd();
-            string[] newline = line.Split();
-            var list = new List<string>();
+
+         
+            var lines =File.ReadAllText(path);
+            Console.WriteLine(lines);
+
+       
+            string[] newline = lines.Split();
             var list1 = new List<int>();
-           foreach(var content in newline)
+            foreach (var content in newline)
             {
-                list.Add(content);
+                //list.Add(content);
                 list1.Add(content.Length);
             }
             Console.WriteLine("Contents of the File:-");
-           foreach(var content in list)
+            foreach (var content in )
             {
                 Console.WriteLine(content);
             }
             Console.WriteLine("\n");
             var max = list1.Max();
             Console.WriteLine("The Largest Word in the file is:-");
-            Console.WriteLine(list[list1.IndexOf(max)]);
+            Console.WriteLine(newline[list1.IndexOf(max)]);
         }   
 
     }
