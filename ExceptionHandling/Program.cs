ef889db3a0a6eec9f7d158e01cb4d081a73716c6
Nagesh Nagansur <nageshnagansur@gmail.com>
2020-04-22 22:56:19 +0530

    Added

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Logging
+    {
+        public static string message;
+        public static void logg(string m)
+        {
+            message = m;
+        }
+    }
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            string path = @"D:\Folder1\sample.txt";
+            try
+            {
+                Console.WriteLine(File.ReadAllText(path));
+                //  Console.WriteLine("Deleting the File");
+            }
+          
+            catch(Exception ex)
+            {
+               
+                Console.WriteLine("Exception");
+                Logging.logg(ex.Message);
+                //int a = 0;                                     //Error
+                //int c = 5 / a;
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.GetType());
+            }
+         
+
+            finally
+            {
+
+                Console.WriteLine("Deleting the file");
+            }
+           
+
+
+
+
+        }
+    }
+}
