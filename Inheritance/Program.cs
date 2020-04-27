13f7354d520d55e6e979e14fe8d317bd53ea176a
Nagesh Nagansur <nageshnagansur@gmail.com>
2020-04-27 23:16:37 +0530

    Added

---
+++ Inheritance/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Logger
+    {
+     
+        public void log(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+  
+
+        
+    }
+    public class Dbmigrator:Logger
+    {
+       public void Migrate()
+        {
+            log("Migrating");
+        }
+
+    }
+    public class Installer:Logger
+    {
+        public void installer()
+        {
+            log("Installing tha application");
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)    
+        {
+            var db = new Dbmigrator();
+            db.Migrate();
+            var install = new Installer();
+            install.installer();
+
+        }
+    }
+}
