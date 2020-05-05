929b224775886c1afd1579cfa555c55bd243897b
Nagesh Nagansur <nageshnagansur@gmail.com>
2020-05-05 21:44:48 +0530

    Added

---
+++ MethodHiding/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodHiding
+{  
+    public class Parent
+    {
+    public virtual void test1()      //Overridable
+        {
+            Console.WriteLine("Parent's test1 method called");
+        }
+    public void test2()
+        {
+            Console.WriteLine("Parent's test2 method called");
+        }
+    
+    }
+    public class Child :Parent
+    {
+        
+        public override void test1()
+        {
+            Console.WriteLine("Child's test1 method called");
+        }
+        public new  void test2()  //method hiding
+        { 
+            Console.WriteLine("child's test2 method called");
+        }
+        public void  ptest1()                //2)
+        {
+            base.test1();
+        }
+        public void ptest2()
+        {
+            base.test2();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Child c = new Child();
+            c.test1();
+            c.test2();
+
+
+            //if you want to call parent method too then.1),2)
+            
+            Parent p= new Parent();     //  1)
+            p.test1();
+            p.test2();
+
+
+
+
+
+        }
+    }
+}
---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,44 @@
+﻿
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace MethodOverriding
+    {
+        public class ParentClass
+        {
+            public void Show()
+            {
+                Console.WriteLine("Parent show method called");
+            }
+            public virtual void Test()
+            {
+                Console.WriteLine("Parent test method called");
+            }
+
+        }
+        public class ChildClass : ParentClass
+        {
+            public override void Test()
+            {
+                Console.WriteLine("Child's test method called");
+            }
+
+        }
+
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                ChildClass c = new ChildClass();
+                c.Show();
+                c.Test();
+
+
+            }
+        }
+    }
+
