22b3f72978e3c0deb295bd847ec25e16d487455c
Nagesh Nagansur <nageshnagansur@gmail.com>
2020-05-09 07:37:47 +0530

    Added

---
+++ AbstractClassEg-1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassEg_1
+{
+    public abstract class Parent
+    {
+        public abstract void Function(int x);
+    
+    }
+    public class Child1 : Parent
+    {
+        public override void Function(int a)
+        {
+            Console.WriteLine("SQR-{0}",a*a);
+        }
+
+    }
+    public class Child2 : Parent
+    
+    {
+        public override void Function(int x)
+        {
+            Console.WriteLine("Cube-{0}",x*x*x);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         //   Parent p1 = new Parent();
+
+            Child1 c1 = new Child1();
+            c1.Function(10);
+            Child2 c2 = new Child2();
+            c2.Function(2);
+
+        }
+    }
+}
---
+++ SealedClass/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedClass
+{
+public  class SystemInformation
+    {
+        string DeviceName = "Acer Nitro 5";
+      
+        public virtual void Info()
+        {
+            Console.WriteLine(DeviceName);
+          
+        }
+        
+
+    }
+    public  class System : SystemInformation
+    {
+
+        //public sealed override void Info()
+        //{
+        //    Console.WriteLine("System");
+        //}
+
+    }
+
+    public class Subsystem : System
+    {
+        public override void Info()
+        {
+            Console.WriteLine("SubSystem");
+        }
+
+
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SystemInformation s = new SystemInformation();
+            s.Info();
+            System s1 = new System();
+            s1.Info();
+            Subsystem s3 = new Subsystem();
+            s3.Info();
+        }
+    }
+}
