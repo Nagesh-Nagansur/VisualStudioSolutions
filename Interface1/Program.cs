827a5f61cf651a94da0cb6f799c1081df63e3791
Nagesh Nagansur <nageshnagansur@gmail.com>
2020-05-11 22:45:23 +0530

    added

---
+++ Interface1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface1
+{
+
+
+    public class Parent 
+    {
+      public  void Method()
+        {
+            Console.WriteLine("Parent method called");
+        }
+    }
+    interface Interface1
+    {
+        void test();
+        void show();
+    }
+    interface Interface2
+    {
+        void test();
+       
+    }
+    
+
+
+
+    class Program : Parent,Interface2,Interface1
+    {
+        void Interface1.show()
+        {
+            Console.WriteLine("Show method is implemented");
+        }
+
+        public void test()
+        {
+            Console.WriteLine("Test method is implemented");
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Method();
+            p.test1();
+
+
+
+            Interface1 i1 = new Program();
+
+            i1.show();
+           
+
+
+
+        }
+
+      
+   
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+   public  interface IInterface1
+    {
+        void Add(int a, int b);
+    }
+    interface IInterface2 : IInterface1
+    {
+        void Sub(int a, int b);
+    }
+    class Program :IInterface2
+    {
+
+      public void Add(int a, int b) 
+        { 
+            Console.WriteLine(a+b);
+        }
+       public void Sub(int a,int b)
+        {
+            Console.WriteLine(a-b);
+        }
+
+         static void Main(string[] args)
+        {
+
+            Program p = new Program();
+     
+            
+            p.Sub(20,10);
+            p.Add(10, 20);
+
+
+
+            IInterface2 i = p;
+            i.Add(10, 10);
+            i.Sub(20, 10);
+
+
+
+
+        }
+    }
+}
