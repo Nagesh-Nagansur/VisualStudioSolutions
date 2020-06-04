fedbc8e3f6d1028ff212bafaefbf880b1654c582
Nagesh Nagansur <nageshnagansur@gmail.com>
2020-06-04 10:41:06 +0530

    Added

---
+++ AnonymousMethod/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousMethod
+{
+    public delegate string GreetingsDelegate(string name);
+    class AnonymousMethod
+    {
+        public static string Greetings(string name)
+        {
+            return "hello" + name + "a very good morning!";
+        } 
+      static void Main()
+        {
+
+
+           
+            //Normal Delegate
+            GreetingsDelegate obj = new GreetingsDelegate(Greetings);
+            string str=obj.Invoke("scott");
+            Console.WriteLine(str);
+
+
+
+            //Anonymouse method
+            GreetingsDelegate obj1 = delegate (string name)
+             {
+                 return "hello " + name + " a very good morning!";
+             };
+
+
+            GreetingsDelegate obj2 = (name)=>
+            {
+                return "Hello this is lambda expressin" + name + "ok";
+            };
+
+
+            string st= obj1("Naruto");
+            Console.WriteLine(st);
+
+
+
+        }
+    }
+} 
+
+
+//1) we write a method and then bind it with the delagate.
+//2) In Anonymous method we bind the method dirictly with the instance of a delegate.
---
+++ DelegateFuncAndAction/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateFuncAndAction
+{
+   
+
+    
+    class MethodCollections
+    {
+
+        //Method which returns nothing.
+        public static void PrintText()
+        {
+            Console.WriteLine("Printing Text");
+        }
+        public static void Square(int a)
+        {
+            Console.WriteLine(a*a);
+        }
+        public static void Mult(int a, int b)
+        {
+            Console.WriteLine(a*b);
+        }
+        
+        
+        
+        //Methods with return types..
+        public static string Text()
+        {
+            return "Text";
+        }
+        public static int DisplayAddition(int a, int b)
+        {
+            return a + b;
+        }
+        public static string DisplayNames(string a,string b,string c)
+        {
+            return a + b + c;
+        }
+
+    }
+    class Program
+    {
+      
+         public static void Main(string[] args)
+        {
+            //Without return type.  Action<>
+            Action Printtext =  MethodCollections.PrintText;
+            Action<int> Square =  MethodCollections.Square;
+            Action<int, int> Mult = MethodCollections.Mult;
+
+
+
+            //With return type..
+            Func<string>Text = new Func<string>(MethodCollections.Text);
+            Func<int, int,int> DisplayAdd = new Func<int,int,int>(MethodCollections.DisplayAddition);
+
+
+            //Invoking the methods
+            Printtext();
+            Square(5);
+            Mult(2, 2); 
+
+
+            string s=Text();
+            Console.WriteLine(s);
+            int a= DisplayAdd(25, 65);
+            Console.WriteLine(a);
+
+        }
+    }
+}
